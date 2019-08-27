74676634ff7f862c8843b50b10cc4c86207f26c6
Andrew Lisowski <lisowski54@gmail.com>
2019-08-26 17:58:42 -0700

    update example

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ To check that incremental builds are working I:
 1. `yarn build`
 2. Delete the `.d.ts` file in `dist` (if incremental builds are working the next step should not emit any files because tsbuldinfo thinks they are already on the system)
 3. `yarn build`
+
+To check that the builds are fast run
+
+1. `yarn build` (get initial emit) ~2.5s
+2. `yarn build` (second emit should be much shorter) ~2.5s
+
+Compare this with `tsc`:
+
+1. `yarn tsc -b tsconfig.json --incremental` (first time takes ~2s)
+2. `yarn tsc -b tsconfig.json --incremental` (second time takes 0.31s)
--- compiler.js
+++ compiler.js
@@ -1,4 +1,5 @@
 const ts = require('typescript');
+const path = require('path');
 
 const configPath = ts.findConfigFile('./', ts.sys.fileExists, 'tsconfig.json');
 const tsconfig = ts.getParsedCommandLineOfConfigFile(
@@ -12,9 +13,8 @@ const tsconfig = ts.getParsedCommandLineOfConfigFile(
 const { options, projectReferences, fileNames } = tsconfig;
 const allOptions = {
   ...options,
-  outDir: 'dist',
   incremental: true,
-  emitDeclarationOnly: true
+  tsBuildInfoFile: path.join(__dirname, 'tsconfig.tsbuildinfo')
 };
 
 const host = ts.createIncrementalCompilerHost(allOptions, ts.sys);
@@ -25,16 +25,21 @@ const program = ts.createIncrementalProgram({
   rootNames: fileNames
 });
 
-const { diagnostics } = program.emit();
+const result = program.emit();
+const allDiagnostics = ts
+  .getPreEmitDiagnostics(program)
+  .concat(result.diagnostics);
 
-const formattedDiagnostics = ts.formatDiagnosticsWithColorAndContext(
-  diagnostics,
-  {
-    getCurrentDirectory: () => ts.sys.getCurrentDirectory(),
-    getNewLine: () => ts.sys.newLine,
-    getCanonicalFileName: filename =>
-      ts.sys.useCaseSensitiveFileNames ? filename : filename.toLowerCase()
-  }
-);
+if (allDiagnostics.length) {
+  const formattedDiagnostics = ts.formatDiagnosticsWithColorAndContext(
+    allDiagnostics,
+    {
+      getCurrentDirectory: () => ts.sys.getCurrentDirectory(),
+      getNewLine: () => ts.sys.newLine,
+      getCanonicalFileName: filename =>
+        ts.sys.useCaseSensitiveFileNames ? filename : filename.toLowerCase()
+    }
+  );
 
-console.log(formattedDiagnostics);
+  console.log(formattedDiagnostics);
+}
